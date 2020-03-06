bbe2cf497005bed5935b08c5d315ded61adf7888
Tony Su <peitaosu@163.com>
2020-03-06 22:34:13 +0800

    init project

---
+++ ref.py
@@ -0,0 +1,12 @@
+class ReferenceManager():
+    def __init__(self):
+        self.config = "ref.conf"
+    
+    def AddReferences(self):
+        pass
+
+    def ReduceReferences(self):
+        pass
+
+    def GetListToDelete(self):
+        pass
