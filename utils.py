6b6d2dff2f350c2ea9f144330bb5477e837c5129
Tony Su <peitaosu@163.com>
2020-03-08 21:26:34 +0800

    remove reserve function

--- utils.py
+++ utils.py
@@ -1,21 +1,16 @@
-def reverse(string): 
-    string = string[::-1] 
-    return string 
-
 def reformat_guid(in_guid, rule):
     out_guid = ""
     if rule == "msi_component":
-        out_guid = reverse(in_guid[1:9]) + \
-                 reverse(in_guid[10:14]) + \
-                 reverse(in_guid[15:17]) + \
-                 reverse(in_guid[17:19]) + \
-                 reverse(in_guid[20:22]) + \
-                 reverse(in_guid[22:24]) + \
-                 reverse(in_guid[25:27]) + \
-                 reverse(in_guid[27:29]) + \
-                 reverse(in_guid[29:31]) + \
-                 reverse(in_guid[31:33]) + \
-                 reverse(in_guid[33:35]) + \
-                 reverse(in_guid[35:37])
+        out_guid = in_guid[1:9][::-1] + \
+                 in_guid[10:14][::-1] + \
+                 in_guid[15:17][::-1] + \
+                 in_guid[17:19][::-1] + \
+                 in_guid[20:22][::-1] + \
+                 in_guid[22:24][::-1] + \
+                 in_guid[25:27][::-1] + \
+                 in_guid[27:29][::-1] + \
+                 in_guid[29:31][::-1] + \
+                 in_guid[31:33][::-1] + \
+                 in_guid[33:35][::-1] + \
+                 in_guid[35:37][::-1]
     return out_guid
-
