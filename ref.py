38e4e068486dd35cc7d0f82e6072a0f9e479ce06
Tony Su <peitaosu@163.com>
2020-03-09 18:45:40 +0800

    support load config

--- ref.py
+++ ref.py
@@ -1,7 +1,19 @@
+import os, sys, json
+
 class ReferenceManager():
     def __init__(self):
-        self.config = "ref.conf"
+        self.config_file = "ref.conf"
+        self.config = {}
     
+    def LoadConfig(self, config_file=None):
+        if config_file:
+            self.config_file = config_file
+        if not os.path.isfile(self.config_file):
+            print("[Error]: {} is not exists.".format(self.config_file))
+            return
+        with open(self.config_file) as in_file:
+            self.config = json.load(in_file)
+
     def AddReferences(self):
         pass
 
