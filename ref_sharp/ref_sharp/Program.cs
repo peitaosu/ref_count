3b35c9af91d25e6cc1947b415105a6bb03964d94
Tony Su <peitaosu@163.com>
2020-06-23 22:49:30 +0800

    create csharp project

---
+++ ref_sharp/ref_sharp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ref_sharp
+{
+    class ReferenceManager
+    {
+        public void LoadConfig()
+        {
+            
+        }
+        public bool Install() {
+            return true;
+        }
+        public bool Uninstall()
+        {
+            return true;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReferenceManager refman = new ReferenceManager();
+            refman.LoadConfig();
+            refman.Install();
+            refman.Uninstall();
+        }
+    }
+}
