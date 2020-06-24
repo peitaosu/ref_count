9b82778a3db1aed64cee3dbe2aaf8544780e115a
Tony Su <peitaosu@163.com>
2020-06-24 22:34:47 +0800

    add function definitions and implementations

--- ref_sharp/ref_sharp/Program.cs
+++ ref_sharp/ref_sharp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,17 +9,54 @@ namespace ref_sharp
 {
     class ReferenceManager
     {
+        private string msi_key_string = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Installer\\UserData\\S-1-5-18\\Components\\";
+
         public void LoadConfig()
         {
             
         }
-        public bool Install() {
+        public bool Install()
+        {
             return true;
         }
         public bool Uninstall()
         {
             return true;
         }
+        public void AddReferences()
+        {
+
+        }
+        public void ReduceReferences()
+        {
+
+        }
+        private bool _add_count_in_registry(string component, string product, string file)
+        {
+            RegistryKey component_key = Registry.LocalMachine.OpenSubKey(msi_key_string + component, true);
+            component_key.SetValue(product, file);
+            component_key.Close();
+            return true;
+        }
+
+        private bool _reduce_count_in_registry(string component, string product)
+        {
+            RegistryKey component_key = Registry.LocalMachine.OpenSubKey(msi_key_string + component, true);
+            component_key.DeleteValue(product);
+            component_key.Close();
+            return true;
+        }
+
+        private int _get_count_in_registry(string component)
+        {
+            return 0;
+        }
+
+        private bool _delete_count_in_registry(string component, string product)
+        {
+            return true;
+        }
+
     }
 
     class Program
